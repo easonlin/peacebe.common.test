1eb2f6bd757686f714adcf39996a10c00a9e7bd1
smartsgy <smartsgy@gmail.com>
2013-06-20 01:51:43 +0800

    [Add] init commit

---
+++ src/peacebe/common/test/DummyActivity.java
@@ -0,0 +1,10 @@
+package peacebe.common.test;
+
+import android.app.Activity;
+import peacebe.common.IPeaceBeServer;
+import peacebe.common.PeaceBeServer;
+
+public class DummyActivity extends Activity {
+
+	IPeaceBeServer srv = new PeaceBeServer();
+}
---
+++ src/peacebe/common/test/PeaceBeServerTestCase.java
@@ -0,0 +1,95 @@
+package peacebe.common.test;
+
+import peacebe.common.IPeaceBeServer;
+import peacebe.common.PeaceBeServer;
+import android.test.AndroidTestCase;
+
+public class PeaceBeServerTestCase extends AndroidTestCase {
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testSendVote() {
+		IPeaceBeServer srv = new PeaceBeServer();
+		fail("Not yet implemented");
+	}
+
+	public void testSendProfile() {
+		fail("Not yet implemented");
+	}
+
+	public void testSendPaint() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartVote() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartGrouping() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartProfiling() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartFinish() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartProfilingFinish() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetState() {
+		System.out.print("HI");
+		fail("Not yet implemented");
+	}
+
+	public void testGetTeamState() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetGroupingResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCandidate() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetProfiled() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPainted() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetVoted() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetTotalResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetTeam() {
+		fail("Not yet implemented");
+	}
+
+}
